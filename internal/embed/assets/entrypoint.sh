5539bd93e3e9f6551b9e9fb2a96c2a463fc806fa
jiriks74 <jiri@stefka.eu>
2026-07-27 15:54:48 +0200

    feat(internal/compose): Allow localhost gateway traffic

--- internal/embed/assets/entrypoint.sh
+++ internal/embed/assets/entrypoint.sh
@@ -47,7 +47,7 @@ fi
 
 HOST_IP=$(getent hosts host.docker.internal | awk '{print $1}' || true)
 if [ -n "$HOST_IP" ]; then
-  $IPT -I OUTPUT -d "$HOST_IP" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+  $IPT -I OUTPUT -d "$HOST_IP" -j ACCEPT
 fi
 
 for GW in $(awk '$3 != "00000000" && $3 != "Gateway" {print $3}' /proc/net/route | sort -u); do
